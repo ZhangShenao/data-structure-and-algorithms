6ade4b087b4844ccbfb6328f9939d22f42bf2938
ZhangShenao <15201440436@163.com>
2023-08-23 10:45:09 +0800

    Reverse K-Group in Single List

--- README.md
+++ README.md
@@ -707,3 +707,88 @@ public class SingleListStack<T> {
     }
 }
 ```
+
+## 算法：K 个一组翻转链表
+
+Leetcode 地址：https://leetcode.cn/problems/reverse-nodes-in-k-group/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+
+**算法思想：从头开始遍历链表，每次找到 K 个一组的节点范围 [start,end]，在组内进行节点指针的翻转。**
+
+算法实现：
+
+```Java
+/**
+ * K个一组翻转链表
+ */
+public ListNode reverseKGroup(ListNode head, int k) {
+    //边界条件校验
+    if (head == null || k <= 0) {
+        return head;
+    }
+    
+    //先找到第一个分组
+    ListNode start = head;
+    ListNode end = findKGroupEnd(start, k);
+    if (end == null) {
+        //节点数量不足k个,无需翻转,直接返回原始头结点
+        return head;
+    }
+    
+    //新的头结点一定是end
+    head = end;
+    
+    //先反转第一组
+    reverse(start, end);
+    
+    //继续遍历链表后续节点,依次处理后面的组
+    ListNode lastGroupEnd = start;
+    while (lastGroupEnd.next != null) {
+        start = lastGroupEnd.next;
+        end = findKGroupEnd(start, k);
+        if (end == null) {
+            return head;
+        }
+        reverse(start, end);
+        
+        //将上一组的尾节点指向当前组的头结点
+        lastGroupEnd.next = end;
+        
+        //继续处理下一组
+        lastGroupEnd = start;
+    }
+    
+    return head;
+}
+
+/**
+ * 找到组内的尾节点end,形成[start,end]区间内的K个节点组
+ */
+private ListNode findKGroupEnd(ListNode start, int k) {
+    while (--k > 0 && start != null) {
+        start = start.next;
+    }
+    return start;
+}
+
+/**
+ * 在[start,end]节点区间内翻转链表
+ */
+private void reverse(ListNode start, ListNode end) {
+    //首先记录下一组的头结点
+    end = end.next;
+    
+    ListNode cur = start;
+    ListNode prev = null;
+    ListNode next = null;
+    
+    while (cur != end) {
+        next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    
+    //将当前组的尾节点指向下一组的头结点
+    start.next = end;
+}
+```
---
+++ leetcode/src/main/java/william/leetcode/Solution25.java
@@ -0,0 +1,105 @@
+package william.leetcode;
+
+/**
+ * @author ZhangShenao
+ * @date 2023/8/23 9:54 AM
+ * @description:
+ * https://leetcode.cn/problems/reverse-nodes-in-k-group/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+ * <p>K个一组翻转链表</p>
+ */
+public class Solution25 {
+    
+    private static class ListNode {
+        
+        int val;
+        
+        ListNode next;
+        
+        ListNode() {
+        }
+        
+        ListNode(int val) {
+            this.val = val;
+        }
+        
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    
+    /**
+     * K个一组翻转链表
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        //边界条件校验
+        if (head == null || k <= 0) {
+            return head;
+        }
+        
+        //先找到第一个分组
+        ListNode start = head;
+        ListNode end = findKGroupEnd(start, k);
+        if (end == null) {
+            //节点数量不足k个,无需翻转,直接返回原始头结点
+            return head;
+        }
+        
+        //新的头结点一定是end
+        head = end;
+        
+        //先反转第一组
+        reverse(start, end);
+        
+        //继续遍历链表后续节点,依次处理后面的组
+        ListNode lastGroupEnd = start;
+        while (lastGroupEnd.next != null) {
+            start = lastGroupEnd.next;
+            end = findKGroupEnd(start, k);
+            if (end == null) {
+                return head;
+            }
+            reverse(start, end);
+            
+            //将上一组的尾节点指向当前组的头结点
+            lastGroupEnd.next = end;
+            
+            //继续处理下一组
+            lastGroupEnd = start;
+        }
+        
+        return head;
+    }
+    
+    /**
+     * 找到组内的尾节点end,形成[start,end]区间内的K个节点组
+     */
+    private ListNode findKGroupEnd(ListNode start, int k) {
+        while (--k > 0 && start != null) {
+            start = start.next;
+        }
+        return start;
+    }
+    
+    /**
+     * 在[start,end]节点区间内翻转链表
+     */
+    private void reverse(ListNode start, ListNode end) {
+        //首先记录下一组的头结点
+        end = end.next;
+        
+        ListNode cur = start;
+        ListNode prev = null;
+        ListNode next = null;
+        
+        while (cur != end) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        
+        //将当前组的尾节点指向下一组的头结点
+        start.next = end;
+    }
+}
